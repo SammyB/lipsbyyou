6583736e7628d1c7fdfb0307142b0ad465949d3b
visualjazz-sbrunno <sbrunno@visualjazz.com.au>
2014-05-09 14:46:30 +1000

    few more tweaks

--- lookingHOT/index.php
+++ lookingHOT/index.php
@@ -397,7 +397,8 @@ if(!file_exists('counter.txt')){
 									</div>
 									<div class="description">
 										Let's get jiggy with it<br>
-										<span>Colour and Flavour-up your balm with something tantalising</span>
+										<span>Colour and Flavour-up your balm with something tantalising</span><br>
+										<div class="tooltip-info"><span class="icon"></span> Select up to two, if you're wanting to mix it up!</div>
 									</div>
 
 									<!-- start balms -->
